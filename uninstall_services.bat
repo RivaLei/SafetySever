50c2ef9576198b83b30b738020d55905e845e026
RivaLei <38826652+RivaLei@users.noreply.github.com>
2018-11-13 17:21:36 +0800

    Add files via upload

---
+++ uninstall_services.bat
@@ -0,0 +1,7 @@
+start /wait D:\Program\wamp64\wampmanager.exe -quit -id={wampserver}
+NET STOP wampapache64
+sc delete wampapache64
+NET STOP wampmysqld64
+sc delete wampmysqld64
+NET STOP wampmariadb64
+sc delete wampmariadb64
